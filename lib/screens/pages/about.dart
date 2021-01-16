32f022eb40672a173a5222cad8a820bfbc2c84c0
DELL <amine.m.a1998@gmail.com>
2021-01-16 11:29:44 +0100

    add pages

---
+++ lib/screens/pages/about.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class ABOUTUS extends StatefulWidget {
+  @override
+  _ABOUTUSState createState() => _ABOUTUSState();
+}
+
+class _ABOUTUSState extends State<ABOUTUS> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.white70,
+      ),
+    );
+  }
+}
